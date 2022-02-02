6d6c057088e83cc5ca9b0ab372446e69ffee78de
icebarf <sysgrammer@protonmail.com>
2022-02-02 10:35:45 +0530

    header file for cross-file usage

---
+++ src/chip.h
@@ -0,0 +1,28 @@
+#ifndef REBORN_CHIP_H
+#define REBORN_CHIP_H
+
+#include <SDL2/SDL.h>
+
+struct chip8_sys {
+
+    const enum constants {
+        MEMSIZE = 4096,
+        REGNUM = 16,
+        STACKSIZE = 48,
+        DISPLAY_SIZE = 64 * 32
+    } constants;
+
+    uint8_t memory[MEMSIZE];
+    uint16_t stack[STACKSIZE];
+    uint8_t registers[REGNUM];
+    uint8_t display[DISPLAY_SIZE];
+
+    uint16_t index;
+    uint16_t program_counter;
+    SDL_atomic_t delay_timer;
+    SDL_atomic_t sound_timer;
+
+    int stacktop;
+};
+
+#endif
