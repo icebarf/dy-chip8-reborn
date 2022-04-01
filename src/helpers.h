87f3fc03178075e3f55be1c3ad7bcb9e9c3b5260
Amritpal <sysgrammer@protonmail.com>
2022-04-01 12:21:51 +0530

    clean this stupidity

--- src/helpers.h
+++ src/helpers.h
@@ -2,10 +2,9 @@
 #define REBORN_HELPERS_H
 #include <stdint.h>
 
-/* prints the memory where the font and ROM are loaded at */
 void print_program_memory(struct chip8_sys* chip8);
 
-/* Push and pop from the stack */
 uint16_t pop(struct chip8_sys* chip8);
 void push(struct chip8_sys* chip8, uint16_t x);
+
 #endif
