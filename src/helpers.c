db13d073513038a26b28b80587365d1029c2f161
icebarf <sysgrammer@protonmail.com>
2022-04-16 10:05:42 +0530

    time delta calculation function defined

--- src/helpers.c
+++ src/helpers.c
@@ -1,4 +1,5 @@
 #include "chip.h"
+#include <SDL2/SDL_timer.h>
 #include <stdio.h>
 
 uint16_t pop(struct chip8_sys* chip8)
@@ -9,4 +10,10 @@ uint16_t pop(struct chip8_sys* chip8)
 void push(struct chip8_sys* chip8, const uint16_t x)
 {
     chip8->stack[++chip8->stacktop] = x;
+}
+
+double get_delta_time(const double current_counter_val, const double prev_counter_val)
+{
+    return ((current_counter_val - prev_counter_val) * 1000000000.0) /
+           SDL_GetPerformanceFrequency();
 }
--- src/helpers.h
+++ src/helpers.h
@@ -2,7 +2,17 @@
 #define REBORN_HELPERS_H
 #include <stdint.h>
 
+/* takes in a pointer to chip8 instance, returns the topmost value from stack,
+ * decrements the  stacktop */
 uint16_t pop(struct chip8_sys* chip8);
+
+/* takes in a pointer to chip8 instance, and value to-be-pushed onto the stack
+ * first increments the stacktop and then stores the value at STACK[stacktop] */
 void push(struct chip8_sys* chip8, const uint16_t x);
+
+/* takes in the current and previous value of the SDL High Performance Counter
+ * Calculates the delta (current - previous)
+ * converts to nanoseconds and then returns the value */
+double get_delta_time(const double current_counter_val, const double prev_counter_val);
 
 #endif
